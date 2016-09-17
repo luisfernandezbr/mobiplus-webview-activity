db4fb0d96b5f81833476c285df8d99b1b49e7461
Luis E. Fernandez <luis.fernandez@mobiplus.com.br>
2016-09-17 20:31:12 -0300

    Sample app now is opening the MPWebViewActivity

--- sample/src/main/java/br/com/mobiplus/webviewactivity/sample/MainActivity.java
+++ sample/src/main/java/br/com/mobiplus/webviewactivity/sample/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.mobiplus.webviewactivity.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+
+import br.com.mobiplus.webviewactivity.MPWebViewActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickOpenWebView(View view) {
-
+        EditText textUrl = (EditText) findViewById(R.id.editWebView);
+        MPWebViewActivity.start(this, textUrl.getText().toString());
     }
 }
