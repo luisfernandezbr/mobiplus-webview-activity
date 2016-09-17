3a4722f23a8cf4f93d1402370d9b0c0a7ed3859f
Luis E. Fernandez <luis.fernandez@mobiplus.com.br>
2016-09-17 20:22:30 -0300

    Added static start method to init MPWebViewActivity passing URL

--- webview-activity/src/main/java/br/com/mobiplus/webviewactivity/MPWebViewActivity.java
+++ webview-activity/src/main/java/br/com/mobiplus/webviewactivity/MPWebViewActivity.java
@@ -1,13 +1,33 @@
 package br.com.mobiplus.webviewactivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MPWebViewActivity extends AppCompatActivity {
+
+    private static final String EXTRA_URL = "EXTRA_URL";
+
+    public static void start(Activity activity, String url) {
+        Intent intent = new Intent(activity, MPWebViewActivity.class);
+        intent.putExtra(EXTRA_URL, url);
+        activity.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mp_activity_webview);
+
+        String url = getIntent().getStringExtra(EXTRA_URL);
+        
+        this.loadWebView(url);
+    }
+
+    private void loadWebView(String url) {
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(url);
     }
 }
