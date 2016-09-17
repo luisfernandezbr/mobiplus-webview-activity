40d959ee468617556daaef9191933eba9b688d6e
Luis E. Fernandez <luis.fernandez@mobiplus.com.br>
2016-09-17 20:17:40 -0300

    Creating WebviewActivity structure

--- webview-activity/src/main/java/br/com/mobiplus/webviewactivity/MPWebViewActivity.java
+++ webview-activity/src/main/java/br/com/mobiplus/webviewactivity/MPWebViewActivity.java
@@ -1,14 +1,13 @@
-package br.com.mobiplus.webviewactivity.sample;
+package br.com.mobiplus.webviewactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MPWebViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mp_activity_webview);
     }
 }
